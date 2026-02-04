89a0ad94852ef31cd16bf6d8032c616686026128
shuhei <hogehoge@hogehoge.com>
2026-02-05 08:57:34 +0900

    フィルタレイアウトの整理

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -189,6 +189,7 @@ def test_assets_layout_redmine_style():
     assert "assets-layout" in res.text
     assert "filter-panel" in res.text
     assert "フィルタ" in res.text
+    assert "状態" in res.text
     assert "資産No" in res.text
     assert "チケット一覧" not in res.text
     assert "list-scroll" in res.text
